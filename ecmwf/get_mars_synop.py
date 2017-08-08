0245ae4818c4bb64d5853ae4f34bdd18de165151
Ag Stephens <ag.stephens@stfc.ac.uk>
2017-08-08 17:08:21 +0100

    Created get_mars_synop.py script
    Added script to get SYNOP data from the ECMWF MARS service.

---
+++ ecmwf/get_mars_synop.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python2.7
+
+'''
+Code to download and transfer the MARS SYNOP data from 
+ECMWF to the JASMIN GWS for Copernicus C3S 311a Lot2
+
+lifted and adapted from
+     https://software.ecmwf.int/wiki/display/WEBAPI/Python+ERA-interim+examples 
+
+But wanting MARS access
+     https://software.ecmwf.int/wiki/display/WEBAPI/Access+MARS
+
+14/6/2017 RJHD, Exeter
+
+-------------------------------------------
+
+To find observations online:
+
+Mars Catalogue / Operational Archive / Atmospheric Model / 1 / Observations-Observations / Year / Month / Conventional Data / 
+
+select day, time and observation type (scroll to Synop)
+'''
+
+import os
+import datetime as dt
+import calendar
+import numpy as np
+
+from ecmwfapi import ECMWFService
+
+server = ECMWFService("mars") # want the MARS archive
+
+# JASMIN group work space
+GWS = "/group_workspaces/jasmin2/c3s311a_lot2/data/incoming/"
+outdir = "mars/"
+version = "v20170628/"
+
+"""
+OBSTYPE:
+
+https://software.ecmwf.int/wiki/display/UDOC/Identification+keywords#Identificationkeywords-obstype
+
+Land surface and sea surface data types are:
+LSD - 1/2/3/4/140/
+SSD - 9/11/12/13/14/19/20/21/22/23/
+
+LK said not to worry about Reduced COADS data - #20
+"""
+
+
+#*************************************************************
+def run_mars_request(start, end, outfile):
+    """
+    Run MARS request and save the output to `outfile`.
+    
+    :param start: start date
+    :param end: end date
+    :return: None
+    """
+    mars_request = {
+            # Specify the ERA-Interim data archive. Don't change.
+            "class": "od", # operational data
+            "expver": "1", # version of the data
+            "date": "{}/to/{}".format(start,end), # date range to extract
+            "obsgroup": "con", # conventional observations
+            "obstype": "1/2/3/4/140/9/11/12/13/14/19/21/22/23", 
+            "stream": "oper", # operational atmospheric model
+            "time": "00:00:00/03:00:00/06:00:00/09:00:00/12:00:00/15:00:00/18:00:00/21:00:00", # all 3 hourly timesteps
+            "type": "ob", # observations
+            }
+            
+    server.execute(mars_request, outfile)
+    return
+
+#*************************************************************
+#*************************************************************
+SplitMonths = False
+
+
+# spin through each year
+for year in range(1979, dt.datetime.now().year + 1):
+
+
+    if SplitMonths:
+
+        # spin through each month
+        for month in (1 + np.arange(12)):
+
+            # set up the start and end dates in correct format
+            start = dt.datetime.strftime(dt.datetime(year,month,1), "%Y-%m-%d")
+            end = dt.datetime.strftime(dt.datetime(year,month,calendar.monthrange(year,month)[1]), "%Y-%m-%d")
+
+            # make output filename
+            filename = "mars_{}.bufr".format(dt.datetime.strftime(dt.datetime(year,month,1), "%Y_%m"))
+
+            # replace with run request and GWS once ready
+            print start, end, filename
+
+            run_mars_request(start, end, os.path.join(GWS, outdir, version, "data", filename))      
+
+            break
+    else:
+        
+        start = dt.datetime.strftime(dt.datetime(year,1,1), "%Y-%m-%d")
+        end = dt.datetime.strftime(dt.datetime(year,12,31), "%Y-%m-%d")
+
+        # make output filename
+        filename = "mars_{}.bufr".format(dt.datetime.strftime(dt.datetime(year,month,1), "%Y"))
+
+        # replace with run request and GWS once ready
+        print start, end, filename
+
+        run_mars_request(start, end, os.path.join(GWS, outdir, version, "data", filename))      
+
+        break
+
+print "finished"
+
+#*************************************************************
