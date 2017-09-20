7d0e63821b6cc7cf24b340f94432bce57934cec5
Robert Dunn <robert.dunn@metoffice.gov.uk>
2017-09-20 11:46:39 +0100

    Added option to download data from ERA-40 inputs

--- ecmwf/get_mars_synop.py
+++ ecmwf/get_mars_synop.py
@@ -49,80 +49,95 @@ Liz Kent said not to worry about Reduced COADS data - #20
 
 
 #*************************************************************
-def run_mars_request(start, end, outfile):
+def run_mars_request(start, end, outfile, datatype = "synop"):
 
-    server.execute({
-            # Specify the ERA-Interim data archive. Don't change.
-            "class": "od", # operational data
-            "expver": "1", # version of the data
-            "date": "{}/to/{}".format(start,end), # date range to extract
-            "obsgroup": "con", # conventional observations
-            "obstype": "1/2/3/4/140/9/11/12/13/14/19/21/22/23", 
-            "stream": "oper", # operational atmospheric model
-            "time": "00:00:00/03:00:00/06:00:00/09:00:00/12:00:00/15:00:00/18:00:00/21:00:00", # all 3 hourly timesteps
-            "type": "ob", # observations
-            },
-                   outfile
-                   )
+    if datatype == "synop":
+        server.execute({
+                # Specify the ERA-Interim data archive. Don't change.
+                "class": "od", # operational data
+                "expver": "1", # version of the data
+                "date": "{}/to/{}".format(start,end), # date range to extract
+                "obsgroup": "con", # conventional observations
+                "obstype": "1/2/3/4/140/9/11/12/13/14/19/21/22/23", 
+                "stream": "oper", # operational atmospheric model
+                "time": "00:00:00/03:00:00/06:00:00/09:00:00/12:00:00/15:00:00/18:00:00/21:00:00", # all 3 hourly timesteps
+                "type": "ob", # observations
+                },
+                       outfile
+                       )
 
+    if datatype == "era40":
+        server.execute({
+                # Specify the ERA-Interim data archive. Don't change.
+                "class": "e4", # ERA40 data
+                "expver": "1", # version of the data
+                "date": "{}/to/{}".format(start,end), # date range to extract
+                "obstype": "1/2/3/4/140/9/11/12/13/14/19/21/22/23", 
+                "stream": "oper", # operational atmospheric model
+                "repres": "bu" # representation of archived data (Adrian Simmons used this in his request, 19/9/17)
+                "time": "00:00:00/03:00:00/06:00:00/09:00:00/12:00:00/15:00:00/18:00:00/21:00:00", # all 3 hourly timesteps
+                "type": "ai", # observations
+                },
+                       outfile
+                       )
     return # run_mars_request
 
 #*************************************************************
 #*************************************************************
 SplitMonths = False
 
-today = dt.datetime.now()
+datatype = "synop"
+datatype = "era40"
 
+if datatype == "synop":
+    start = 1979
+elif datatype == "era40":
+    start = 1957
 
 # spin through each year
-#for year in range(1979, dt.datetime.now().year + 1):
-for year in range(2017, dt.datetime.now().year + 1):
+#for year in range(start, dt.datetime.now().year + 1):
+for year in range(1960, 1961):
 
 
     if SplitMonths:
 
         # spin through each month
         for month in (1 + np.arange(12)):
-	
-            start = dt.datetime.strftime(dt.datetime(year,month,1), "%Y-%m-%d")
-	    if year == today.year and month == today.month:
-                end = dt.datetime.strftime(today, "%Y-%m-%d")
-	    elif year == today.year and month > today.month:
-	        break	
-	    else:
-                end = dt.datetime.strftime(dt.datetime(year,month,calendar.monthrange(year,month)[1]), "%Y-%m-%d")
-
-	    	
 
             # set up the start and end dates in correct format
             start = dt.datetime.strftime(dt.datetime(year,month,1), "%Y-%m-%d")
             end = dt.datetime.strftime(dt.datetime(year,month,calendar.monthrange(year,month)[1]), "%Y-%m-%d")
 
             # make output filename
-            filename = "mars_{}.bufr".format(dt.datetime.strftime(dt.datetime(year,month,1), "%Y_%m"))
+            if datatype == "synop":
+                filename = "mars_{}.bufr".format(dt.datetime.strftime(dt.datetime(year,month,1), "%Y_%m"))
+            elif datatype == "era40":
+                filename = "mars_era40_{}.bufr".format(dt.datetime.strftime(dt.datetime(year,month,1), "%Y_%m"))
+                
 
             # replace with run request and GWS once ready
             print start, end, filename
 
-            run_mars_request(start, end, os.path.join(GWS, outdir, version, "data", filename))      
+            run_mars_request(start, end, os.path.join(GWS, outdir, version, "data", filename, datatype = datatype))      
 
+            break
     else:
         
         start = dt.datetime.strftime(dt.datetime(year,1,1), "%Y-%m-%d")
-	
-	if year == today.year:
-            end = dt.datetime.strftime(today, "%Y-%m-%d")
-	else:
-            end = dt.datetime.strftime(dt.datetime(year,12,31), "%Y-%m-%d")
+        end = dt.datetime.strftime(dt.datetime(year,12,31), "%Y-%m-%d")
 
         # make output filename
-        filename = "mars_{}.bufr".format(dt.datetime.strftime(dt.datetime(year,1,1), "%Y"))
+        if datatype == "synop":
+            filename = "mars_{}.bufr".format(dt.datetime.strftime(dt.datetime(year,month,1), "%Y"))
+        elif datatype == "era40":
+            filename = "mars_era40_{}.bufr".format(dt.datetime.strftime(dt.datetime(year,month,1), "%Y"))
 
         # replace with run request and GWS once ready
         print start, end, filename
 
-        run_mars_request(start, end, os.path.join(GWS, outdir, version, "data", filename))      
+        run_mars_request(start, end, os.path.join(GWS, outdir, version, "data", filename, datatype = datatype))      
 
+        break
 
 print "finished"
 
